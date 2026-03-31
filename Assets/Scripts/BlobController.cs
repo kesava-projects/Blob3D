c253ef1fd2111afd136316e25c123614dd07daa0
kesava-projects <kesavaharshavardhan@gmail.com>
2026-03-31 12:54:43 +0530

    speed inc

--- Assets/Scripts/BlobController.cs
+++ Assets/Scripts/BlobController.cs
@@ -208,7 +208,7 @@ public class BlobController : MonoBehaviour
         }
 
         Vector3 moveDir = CharacterPlanarForward();
-        rb.AddForce(moveDir * input.y * moveSpeed, ForceMode.Acceleration);
+        rb.AddForce(moveDir * input.y * moveSpeed, ForceMode.Force);
 
         Vector3 hv = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
         if (hv.magnitude > maxSpeed)
